ed89dbc6e0c35b35aa514af6cc48887dc296956a
easyhoom <1813855384@qq.com>
2022-03-22 19:53:40 +0800

    提交代码

---
+++ Project1/Project1/源.cpp
@@ -0,0 +1,126 @@
+//
+//#include<iostream>
+//#include<iomanip>
+//using namespace std;
+//int main() {
+//    double x, y;
+//    cin >> x;
+//    if (x < 2)  y = x;
+//    if (x>=2 and x < 10)  y = 2 * x + 3;
+//    if (x>=10 and x < 36)  y = 3 * x - 8;
+//    if (x >= 36)  y = 4 * x - 11;
+//    cout << fixed << setprecision(4) << y << endl;
+//    return 0;
+//}
+// 
+// 3.
+//#include<iostream>
+//#include<stdio.h>
+//#include<stdlib.h>
+//using namespace std;
+//int main() {
+//	double x;
+//	cin >> x;
+//	int grade = int(x / 10);
+//	switch (grade) {
+//	case  10:
+//		cout << "A";
+//		break;
+//	case 9:
+//		cout << "A";
+//		break;
+//	case 8:
+//		cout << "B";
+//		break;
+//	case 7:
+//		cout << "C";
+//		break;
+//	case 6:
+//
+//		cout << "D";
+//
+//		break;
+//	case 5:
+//		cout << "E";
+//		break;
+//	case 4:
+//		cout << "E";
+//		break;
+//	case 3:
+//		cout << "E";
+//		break;
+//	case 2:
+//		cout << "E";
+//		break;
+//	case 1:
+//		cout << "E";
+//		break;
+//	case 0:
+//		cout << "E";
+//		break;
+//	}
+//
+//
+//
+//	}
+//
+////}
+
+
+//4.
+//#include<iostream>
+//
+//using namespace std;
+//int main() {
+//    int x,y;
+//    cin >> x;
+//    y = x%4;
+//    if(y == 0)
+//    {
+//        cout << "IsLeapYear";
+//    }
+//    else if(y>0){ 
+//        cout << "NotLeapYear"; 
+//    }
+//        
+//    return 0;
+//        
+//}
+// 
+// 
+// 5.
+//#include<iostream>
+//using namespace std;
+//double y;
+//int
+//#include<iostream>为什么vs编译器中不行？
+//using namespace std;
+//inline double Add(double x, double y);
+//int main(){
+//    double a, b;
+//    cin >> a>> b;
+//    cout << Add(a, b) << endl;
+//}
+//inline double Add(double x, double y) {
+//    return x + y;
+//}
+//6.
+//#include<iostream>
+//using namespace std;
+//int main() {
+//	double sum=0;
+//	int n;
+//	cin >> n;
+//	while (n > 0) {
+//		sum += 1.0 / n;
+//		n -= 1;
+//
+//	}
+//
+//	cout << sum << endl;
+//
+//}
+
+
+
+
