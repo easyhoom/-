1a04577c22c0877d519ef649b87da5ca6dfa3b1f
easyhoom <1813855384@qq.com>
2022-04-03 22:18:30 +0800

    提交代码

---
+++ 作业3/作业3/源.cpp
@@ -0,0 +1,45 @@
+﻿//实现客户机(Client)类。定义字符型静态数据成员name，保存其服务器名称；
+//整型静态数据成员num，记录已定义的客户机数量；定义静态函数changeName()改变服务器名称。
+//完成程序，使得提供的测试程序可以运行并得到要求的结果
+//主main()如下：‪‬‪‬‪‬‪‬‪‬‮‬‭‬‫‬‪‬‪‬‪‬‪‬‪‬‮‬‫‬‫‬‪‬‪‬‪‬‪‬‪‬‮‬‪‬‭‬‪‬‪‬‪‬‪‬‪‬‮‬‫‬‫‬‪‬‪‬‪‬‪‬‪‬‮‬‪‬‮‬‪‬‪‬‪‬‪‬‪‬‮‬‫‬‫‬
+
+/*在这里完成类代码*/
+
+//在这里给出函数被调用进行测试的例子。例如：
+//int main() {
+//    Client::show();
+//    Client c1;
+//    Client::show();
+//    Client::changeName('B');
+//    Client::show();
+//    return 0;
+//}
+#include<iostream>
+#include<string>
+using namespace std;
+class Client {
+public: 
+	Client() {
+		++num;
+	}
+	static string name;
+	static int num;
+	static void  changeName(string a) {
+		name = a;
+	}
+	static void show() {
+		cout << "server name:" << name << endl;
+		cout << "num of clients:" << num << endl;
+	}
+};
+string Client::name = "A";
+int Client::num = 0;
+int main() {
+	Client::show();
+	Client c1;
+	Client::show();
+	Client::changeName("B");
+	Client::show();
+	return 0;
+}
+
---
+++ 作业4/作业4/源.cpp
@@ -0,0 +1,72 @@
+﻿//幼儿园小班的小朋友开始学画画了，小明喜欢在白纸上画点点，请你帮他统计一下点的个数。
+//请定义一个Point类，内有数据成员x，y表示点的坐标，还有一个静态数据成员count用于统计点的个数。
+//并有若干成员函数，如构造函数、显示点的个数的函数showCount()等等。
+//主函数main()如下：‪‬‪‬‪‬‪‬‪‬‮‬‭‬‫‬‪‬‪‬‪‬‪‬‪‬‮‬‫‬‫‬‪‬‪‬‪‬‪‬‪‬‮‬‪‬‭‬‪‬‪‬‪‬‪‬‪‬‮‬‫‬‫‬‪‬‪‬‪‬‪‬‪‬‮‬‪‬‮‬‪‬‪‬‪‬‪‬‪‬‮‬‫‬‫‬
+//
+//int main() {
+//	int x, y;
+//	cin >> x >> y;
+//	Point::showCount();
+//	Point a(x, y);
+//	cout << "Point A: " << a.getX() << "," << a.getY() << endl;
+//	Point::showCount();
+//	Point b(a);
+//	cout << "Point B: " << b.getX() << "," << b.getY() << endl;
+//	Point::showCount();
+//	Point c;
+//	cout << "Point C: " << c.getX() << "," << c.getY() << endl;
+//	Point::showCount();
+//	Point d(x);
+//	cout << "Point D: " << d.getX() << "," << d.getY() << endl;
+//	Point::showCount();
+//	return 0;
+//}
+#include<iostream>
+#include<string>
+using namespace std;
+class Point {
+public:
+	int ox;
+	int oy;
+	static int count;
+	Point(int x2=0, int y2=0):ox(x2),oy(y2){
+		++count;
+	}
+	Point(const Point& p) {
+		ox = p.ox;
+		oy = p.oy;
+		++count;
+	}
+	
+	int getX() {
+
+		return ox;
+	}
+	int getY() {
+		
+		return oy;
+	}
+	static void showCount() {
+		cout << count << endl;
+	}
+
+};
+int Point::count = 0;
+int main() {
+	int x, y;
+	cin >> x >> y;
+	Point::showCount();
+	Point a(x, y);
+	cout << "Point A: " << a.getX() << "," << a.getY() << endl;
+	Point::showCount();
+	Point b(a);
+	cout << "Point B: " << b.getX() << "," << b.getY() << endl;
+	Point::showCount();
+	Point c;
+	cout << "Point C: " << c.getX() << "," << c.getY() << endl;
+	Point::showCount();
+	Point d(x);
+	cout << "Point D: " << d.getX() << "," << d.getY() << endl;
+	Point::showCount();
+	return 0;
+}
